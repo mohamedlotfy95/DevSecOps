b69d14f94c288aef7b58b93a5fc8dc552d45add5
Lotfy <az_lotfy@outlook.com>
2023-03-21 21:05:39 +0200

    editing

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main (void)
 {
-    printf("You God Damm Right \n");
+    string fs = get_string("Say ur naaaaaaaaaaaame ");
+    string lt = get_string("Say ur Father's naaaaaaaaaaame ");
+    printf(" %s %s  \n","You God Damm Right \n");
 }
