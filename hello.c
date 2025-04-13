a0e5710927542c20d040456b403f4bc5d1c86f71
Mohamed Lotfy <mohamed.mohamed.mm939@gmail.com>
2025-04-13 15:04:28 +0200

    Delete hello.c

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main (void)
-{
-    string fs = get_string("Say ur naaaaaaaaaaaame ");
-    string lt = get_string("Say ur Father's naaaaaaaaaaame ");
-    printf(" %s %s  \n","You God Damm Right \n");
-}
